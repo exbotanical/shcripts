0e5c12bbe29dc89285312423b67e005d0d684e1d
goldmund <matthewtzito@gmail.com>
2020-11-19 09:05:20 -0800

    scaffold sh server

---
+++ rest_server.sh
@@ -0,0 +1,31 @@
+#!/bin/bash     
+#title          :rest_server.sh
+#desc           :a simple restful server
+#author         :Matthew Zito (goldmund)
+#created        :11/2020
+#version        :1.0.0  
+#usage          :bash ./rest_server.sh
+#environment    :bash 5.0.17(1)-release
+#===============================================================================
+touch log.txt
+
+while true; do
+    nc -l 5000 > >(
+        while read -r rq; do
+            printf '%s' "$(echo $rq | tr '\r\n' ' ' | tr -d '-')" >> log.txt
+            q=$(echo $rq | tr -d '\r\n')
+
+            if echo "$q" | grep -qE '^GET /'; then
+
+                echo "Request: GET"
+
+            elif echo "$q" | grep -qE '^POST /'; then
+
+                echo "Request: POST"
+
+            fi
+        done
+    echo "" >> log.txt
+    )
+done
+
